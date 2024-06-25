7b08116415fdef69bdc597c9489dd3d37044f0a7
Yusuf <yuzer5252@gmail.com>
2024-06-25 22:18:50 +0300

    first commit ludo africa

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Ludo Africa Server');
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
